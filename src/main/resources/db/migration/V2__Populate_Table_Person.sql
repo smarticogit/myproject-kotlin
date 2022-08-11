8799f14517c8a05a58332818cb6c39a4a5070f0d
daniel.silveira <daniel.silveira@ifood.com.br>
2022-08-10 21:29:16 -0300

    Adicionando Flyway Migration

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES
+    (1,'Ferraz - SP','Jow','Male','Silveira'),
+    (4,'Ferraz - SP','Daniel','Male','Silveira'),
+    (6,'Vila Antonieta','Daniel','Homem','Ribeiro'),
+    (7,'Ferraz - SP','Daniel','Male','Silveira'),
+    (8,'Ferraz - SP','Daniel','Male','Silveira');
