6c2b6ee89ea281e66c65f51f5f6b1cf1f8ac31b8
daniel.silveira <daniel.silveira@ifood.com.br>
2022-08-15 09:49:30 -0300

    Json Serializacao

--- src/main/kotlin/br/com/myproject/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/myproject/data/vo/v1/PersonVO.kt
@@ -1,10 +1,21 @@
 package br.com.myproject.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+//Permite configurar a exibição do Json definindo a ordem dos campos ou até ignorar alguns deles
+@JsonPropertyOrder("id", "address", "first_Name", "lastName", "gender")
 data class PersonVO (
 
     var id: Long = 0,
+
+    @field:JsonProperty("first_Name")
     var firstName: String = "",
+
     var lastName: String = "",
     var address: String = "",
+
+    @field:JsonIgnore
     var gender: String = ""
 )
